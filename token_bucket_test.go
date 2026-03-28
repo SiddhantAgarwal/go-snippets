ed7c046a9b3bf33e9514510afc26eed3bc35efc8
Siddhant Agarwal <agarwal.siddhant23@gmail.com>
2026-03-28 11:07:34 +0530

    token bucket

--- token_bucket_test.go
+++ token_bucket_test.go
@@ -26,21 +26,30 @@ func TestTokenBucket(t *testing.T) {
 }
 
 func TestTokenBucketParallel(t *testing.T) {
-	tb := NewTokenBucket(1, 2)
-	var (
-		allowedCount int64
-		wg           sync.WaitGroup
-	)
+	synctest.Test(t, func(t *testing.T) {
+		tb := NewTokenBucket(1, 2)
+		var (
+			allowedCount int64
+			wg           sync.WaitGroup
+		)
 
-	for range 10 {
-		wg.Go(func() {
-			if tb.Allow() {
-				atomic.AddInt64(&allowedCount, 1)
-			}
-		})
-	}
+		for range 10 {
+			wg.Go(func() {
+				if tb.Allow() {
+					atomic.AddInt64(&allowedCount, 1)
+				}
+			})
+		}
 
-	wg.Wait()
+		wg.Wait()
 
-	assert.Equal(t, int64(2), allowedCount)
+		assert.Equal(t, int64(2), allowedCount)
+
+		time.Sleep(time.Second * 2)
+		if tb.Allow() {
+			atomic.AddInt64(&allowedCount, 1)
+		}
+
+		assert.Equal(t, int64(3), allowedCount)
+	})
 }
